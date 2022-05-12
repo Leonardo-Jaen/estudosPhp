49b575d1c9fb065eef2a55f6f383cd90ad534d8a
Leonardo-Jaen <leco.jaen1@gmail.com>
2022-05-12 18:16:32 -0300

    exemplo01

---
+++ variaveis/exemplo01.php
@@ -0,0 +1,10 @@
+<?php
+// para criar uma variavel com o php, é preciso ultiliar o '$' e, logo após, colocar o nome.
+
+$nome = "leonardo";
+
+var_dump($nome); //var_dump mostra o tipo de dado, o tamanho que ele gastou na memoria e o valor que está dentro da variavel.  
+
+// nós colocamos o sinal de igual na frente da variavel para atribuir um valor para dentro da mesma, ela é um espaço de memoria do servidor.    
+
+?>

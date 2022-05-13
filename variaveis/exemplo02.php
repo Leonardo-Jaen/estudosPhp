955fae3a030ce75a873d3f96bbaff9fa27a453e1
Leonardo-Jaen <leco.jaen1@gmail.com>
2022-05-13 01:10:59 -0300

    estou fazendo exemplo02, fala sobre os tipos de dados

---
+++ variaveis/exemplo02.php
@@ -0,0 +1,11 @@
+<?php
+//Algumas regrar sobre variaveis, nós temos algumas restricoes. Uma boa sugestão a o criar uma variavel é criar usando nomes autoexplicativos. 
+//Existe um padrão chamado camelCase que diz para começar com a primeira letra em minúsculo e, se for uma palavra composta, para cada nome, colocamos letra maiuscula.
+//Um bom exemplo é o nome dos personagens de jogo, se fossemos fazer uma variavel sobre eles, seria assim:
+
+$josephJoestar = "JoJo"; //perceba que o segundo nome começa com a letra maiuscula.
+
+$nome1 = "cindy"; //pode pois o numero está no final da variavel.
+//$1nome = "sla"; não pode, pois vai dar erro, você não podera nem criar.
+$nome_1 = "cley"; //só pode o underline.
+?>
